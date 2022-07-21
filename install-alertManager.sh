68b8e97c84c4d9dbc47881ff2f939e7e384cfa66
udayreddyd <udaykrd33@gmail.com>
2022-07-21 17:44:44 -0500

    Update install-alertManager.sh

--- install-alertManager.sh
+++ install-alertManager.sh
@@ -9,3 +9,8 @@ sudo chown -R alertmanager:alertmanager /etc/alertmanager
 sudo mkdir -p /var/lib/alertmanager
 sudo chown alertmanager:alertmanager /var/lib/alertmanager
 sudo mkdir -p /etc/amtool
+###add config file
+cp alertmanager.service /etc/systemd/system/alertmanager.service
+sudo systemctl enable alertmanager
+sudo systemctl start alertmanager
+sudo systemctl status alertmanager
