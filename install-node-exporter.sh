07281d2a81736eb6eb27a8ee9f95bc4dc56e6fd5
uday <udaykrd33@gmail.com>
2022-07-22 01:04:44 -0500

    Update install-node-exporter.sh

--- install-node-exporter.sh
+++ install-node-exporter.sh
@@ -1,5 +1,7 @@
+sudo yum install -y git
 sudo useradd -M -r -s /bin/false node_exporter
+cd /home/opc/
 wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
 tar xvfz node_exporter-0.18.1.linux-amd64.tar.gz
-sudo cp node_exporter-0.18.1.linux-amd64/node_exporter /usr/local/bin/
+sudo cp /home/opc/node_exporter-0.18.1.linux-amd64/node_exporter /usr/local/bin/
 sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
