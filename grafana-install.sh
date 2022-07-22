6acec259bf331dd979ded82329b6c195cd705c7c
uday <udaykrd33@gmail.com>
2022-07-22 09:33:22 -0500

    Update grafana-install.sh

--- grafana-install.sh
+++ grafana-install.sh
@@ -1,7 +1,7 @@
 sudo yum install -y git
 cd /home/opc/
 wget https://dl.grafana.com/oss/release/grafana-8.1.5-1.x86_64.rpm
-sudo yum install grafana-8.1.5-1.x86_64.rpm
+sudo yum install -y grafana-8.1.5-1.x86_64.rpm
 sudo systemctl enable grafana-server
 sudo systemctl start grafana-server
 sudo systemctl status grafana-server
