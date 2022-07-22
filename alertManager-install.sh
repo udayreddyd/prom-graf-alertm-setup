ffdd983c336f05bee22b94e23b407479c10d071f
uday <udaykrd33@gmail.com>
2022-07-22 01:28:02 -0500

    Update and rename install-alertManager.sh to alertManager-install.sh

--- alertManager-install.sh
+++ alertManager-install.sh
@@ -11,8 +11,7 @@ sudo chown -R alertmanager:alertmanager /etc/alertmanager
 sudo mkdir -p /var/lib/alertmanager
 sudo chown alertmanager:alertmanager /var/lib/alertmanager
 sudo mkdir -p /etc/amtool
-###add config file
-cp /home/opc/prom-graf-alertm-setup/alertmanager.service /etc/systemd/system/alertmanager.service
+sudo cp /home/opc/prom-graf-alertm-setup/alertmanager.service /etc/systemd/system/alertmanager.service
 sudo systemctl enable alertmanager
 sudo systemctl start alertmanager
 sudo systemctl status alertmanager
