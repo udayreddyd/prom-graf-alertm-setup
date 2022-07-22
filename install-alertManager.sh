5aed6b1d4d426210036b8fcb0e07a74a01e8bb93
uday <udaykrd33@gmail.com>
2022-07-22 00:59:36 -0500

    Update install-alertManager.sh

--- install-alertManager.sh
+++ install-alertManager.sh
@@ -1,17 +1,18 @@
+sudo yum install -y git
 sudo useradd -M -r -s /bin/false alertmanager
 cd /home/opc/
 wget https://github.com/prometheus/alertmanager/releases/download/v0.20.0/alertmanager-0.20.0.linux-amd64.tar.gz
 tar xvfz alertmanager-0.20.0.linux-amd64.tar.gz
-sudo cp alertmanager-0.20.0.linux-amd64/{alertmanager,amtool} /usr/local/bin/
+sudo cp /home/opc/alertmanager-0.20.0.linux-amd64/{alertmanager,amtool} /usr/local/bin/
 sudo chown alertmanager:alertmanager /usr/local/bin/{alertmanager,amtool}
 sudo mkdir -p /etc/alertmanager
-sudo cp alertmanager-0.20.0.linux-amd64/alertmanager.yml /etc/alertmanager
+sudo cp /home/opc/alertmanager-0.20.0.linux-amd64/alertmanager.yml /etc/alertmanager
 sudo chown -R alertmanager:alertmanager /etc/alertmanager
 sudo mkdir -p /var/lib/alertmanager
 sudo chown alertmanager:alertmanager /var/lib/alertmanager
 sudo mkdir -p /etc/amtool
 ###add config file
-cp alertmanager.service /etc/systemd/system/alertmanager.service
+cp /home/opc/prom-graf-alertm-setup/alertmanager.service /etc/systemd/system/alertmanager.service
 sudo systemctl enable alertmanager
 sudo systemctl start alertmanager
 sudo systemctl status alertmanager
