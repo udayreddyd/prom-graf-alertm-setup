8a1fe968176e9272a9c813cf74130621c0ea4499
uday <udaykrd33@gmail.com>
2022-07-22 01:21:23 -0500

    Update install-promethes.sh

--- install-promethes.sh
+++ install-promethes.sh
@@ -2,15 +2,15 @@ sudo yum install -y git
 cd /home/opc/
 curl -LO url -LO https://github.com/prometheus/prometheus/releases/download/v2.37.0/prometheus-2.37.0.linux-amd64.tar.gz
 tar -xvf prometheus-2.37.0.linux-amd64.tar.gz
-mkdir -p /home/opc/prometheus-files
-cp -rf /home/opc/prometheus-2.37.0.linux-amd64 prometheus-files
+mkdir -p /home/opc/prometheus-files/
+cp -rf /home/opc/prometheus-2.37.0.linux-amd64/ /home/opc/prometheus-files/
 sudo useradd --no-create-home --shell /bin/false prometheus
 sudo mkdir /etc/prometheus
 sudo mkdir /var/lib/prometheus
 sudo chown prometheus:prometheus /etc/prometheus
 sudo chown prometheus:prometheus /var/lib/prometheus
-sudo cp /home/opc/prometheus-files/prometheus /usr/local/bin/
-sudo cp /home/opc/prometheus-files/promtool /usr/local/bin/
+sudo cp /home/opc/prometheus-files/prometheus-2.37.0.linux-amd64/prometheus /usr/local/bin/
+sudo cp /home/opc/prometheus-files/prometheus-2.37.0.linux-amd64/promtool /usr/local/bin/
 sudo chown prometheus:prometheus /usr/local/bin/prometheus
 sudo chown prometheus:prometheus /usr/local/bin/promtool
 sudo cp /home/opc/prom-graf-alertm-setup/prometheus.yml /etc/prometheus/
