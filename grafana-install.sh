3b93fc02afe2a4aef77ac0117bc2c2a0ce7b6be0
uday <udaykrd33@gmail.com>
2022-07-28 23:50:37 -0500

    Update grafana-install.sh

--- grafana-install.sh
+++ grafana-install.sh
@@ -5,8 +5,14 @@ sudo yum install -y grafana-8.1.5-1.x86_64.rpm
 sudo systemctl enable grafana-server
 sudo systemctl start grafana-server
 sudo systemctl status grafana-server
+##Prometheus
 sudo firewall-cmd --zone=public --add-port=9090/tcp --permanent
+##Grafana
 sudo firewall-cmd --zone=public --add-port=3000/tcp --permanent
+##NodeExporter
 sudo firewall-cmd --zone=public --add-port=7676/tcp --permanent
+##Alert Manager
 sudo firewall-cmd --zone=public --add-port=9093/tcp --permanent
+##Apache
+sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
 sudo firewall-cmd --reload
